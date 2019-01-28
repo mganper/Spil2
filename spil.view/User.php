69e226f03868b8e38205a8ee94a18ba9ce611936
cauni <cauni@DESKTOP-280R1VQ>
2019-01-28 12:59:05 +0100

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	nbproject/project.xml

--- spil.view/User.php
+++ spil.view/User.php
@@ -36,30 +36,35 @@
     </head>
     <body>
         <!--    navbar come here          -->
-        <nav class="navbar navbar-fixed-top navbar-expand-md bg-info">
+        <nav class="navbarnavbar-expand-md bg-info">
             <div class="container">
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar-info" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-bar"></span>
                     <span class="navbar-toggler-bar"></span>
                     <span class="navbar-toggler-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Spil</a>
-                <div class="collapse navbar-collapse" id="navbar-info">
+                <a class="navbar-brand nav-link" href="">Spil</a>                
+                <a class="navbar-brand nav-link" href="#">Inicio</a>
+                <a class="navbar-brand nav-link" href="#">Notificaciones</a>
+                <a class="navbar-brand nav-link" href="#">Perfil</a>
+                <a class="navbar-brand nav-link" href="#">Configuracion</a>
+                <button class="navbar-brand btn" style="margin: 5px; border: none; text-align: right; color: #00bbff; background-color: white;">Spilear</button>
+<!--                <div class="collapse navbar-collapse" id="navbar-info">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item">
                             <a class="nav-link" href="#">Inicio</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Perfil</a>
+                            <a class="nav-link" href="#">Notificaciones</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Notificaciones</a>
+                            <a class="nav-link" href="#">Perfil</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Configuracion</a>
                         </li>
                     </ul>
-                </div>
+                </div>-->
             </div>
         </nav>
         <!-- end navbar  -->
