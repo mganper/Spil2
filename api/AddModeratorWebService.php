e976fbeeeaa94a74ee3196bdee5e500e2b00f4db
D3algr4t <manu.gandul@gmail.com>
2019-01-29 00:39:51 +0100

    Add webservice

--- api/AddModeratorWebService.php
+++ api/AddModeratorWebService.php
@@ -4,14 +4,12 @@ require $_SERVER['DOCUMENT_ROOT'] . '\Spil2\spil.controller\UserControllerImpl.p
 
 session_start();
 
-if(isset($_SESSION['usuario'])){
-    $idUsuario = $_SESSION['usuario'];
-    
+if(isset($_SESSION['usuario'])){    
     $idUsuarioAsc = $_POST['idUsuarioAsc'];
     
     $controller = new UserControllerImpl();
     
-    $resp = ['resp' => $controller->ascenderModerador($idUsuario, $idUsuarioAsc)];
+    $resp = ['resp' => $controller->ascenderModerador($idUsuarioAsc)];
     
     echo json_encode($resp);
 } else {
