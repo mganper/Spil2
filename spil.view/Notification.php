d7c60112708bb6bdd9623aadee30cc78c2a09053
cauni <cauni@DESKTOP-280R1VQ>
2019-01-28 13:23:29 +0100

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	nbproject/project.xml

--- spil.view/Notification.php
+++ spil.view/Notification.php
@@ -1,12 +1,110 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="utf-8" />
+        <link rel="icon" type="image/png" href="assets/img/favicon.ico">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+        <title>Paper Kit 2 by Creative Tim</title>
+
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+        <meta name="viewport" content="width=device-width" />
+
+        <link href="pk2-free-v2.0.1/assets/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="pk2-free-v2.0.1/assets/css/paper-kit.css?v=2.0.1" rel="stylesheet"/>
+
+        <!--     Fonts and icons     -->
+        <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+        <link href="pk2-free-v2.0.1/assets/css/nucleo-icons.css" rel="stylesheet" />
+
+        <style> .navbar {
+                margin-bottom: 0;
+                border-radius: 0;
+            }
+
+            /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+            .row.content {height: 450px}
+
+            /* Set gray background color and 100% height */
+            .sidenav {
+                padding-top: 20px;
+                padding-bottom: 50%;
+                background-color: #f1f1f1;
+            }</style>
+
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <!--    navbar come here          -->
+        <nav class="navbarnavbar-expand-md bg-info">
+            <div class="container" style="text-align: center;">         
+                <img src="pk2-free-v2.0.1/assets/img/spil_favicon_iz.png" style="max-width: 40px">          
+                <a class="navbar-brand nav-link" href="#">Inicio</a>
+                <a class="navbar-brand nav-link" href="#">Notificaciones</a>
+                <a class="navbar-brand nav-link" href="#">Perfil</a>
+                <a class="navbar-brand nav-link" href="#">Configuracion</a>
+                <button class="navbar-brand btn" style="margin: 5px; border: none; text-align: right; color: #00bbff; background-color: white;">Spilear</button>
+                <img src="pk2-free-v2.0.1/assets/img/spil_favicon_de.png" style="max-width: 40px; margin-left: 20px">
+
+            </div>
+        </nav>
+        <!-- end navbar  -->
+
+        <div class="wrapper">
+            <div class="container-fluid text-center">    
+                <div class="row content" style="margin-top: 5px;">
+                    <div class="col-sm-2 sidenav">
+                        <div class="card-block col-sm-12" style="background-color: white; margin-top: 10px;">
+                            <div>
+                                <footer>FOOTER                              </footer>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-8 text-center"> 
+                        <h3>Notificacion 1</h3>
+                        <hr>
+                        <h3>NOTIFICACION 2</h3>
+                        <hr>
+                        <h3>NOTIFICACION N</h3>
+                    </div>
+                    <div class="col-sm-2 sidenav">
+                        <div class="card-block col-sm-11 offset-sm-1" style="background-color: white;">
+                            <div class="info-user ">
+                                <h5>RANK1</h5>
+                                <HR>
+                                <h5>RANK2</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+        <!-- Modal Bodies come here -->
+
+        <!--   end modal -->
+
+
     </body>
+    <!-- Core JS Files -->
+    <script src="pk2-free-v2.0.1/assets/js/jquery-3.2.1.js" type="text/javascript"></script>
+    <script src="pk2-free-v2.0.1/assets/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
+    <script src="pk2-free-v2.0.1/assets/js/tether.min.js" type="text/javascript"></script>
+    <script src="pk2-free-v2.0.1/assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+    <!-- Switches -->
+    <script src="pk2-free-v2.0.1/assets/js/bootstrap-switch.min.js"></script>
+
+    <!--  Plugins for Slider -->
+    <script src="pk2-free-v2.0.1/assets/js/nouislider.js"></script>
+
+    <!--  Plugins for DateTimePicker -->
+    <script src="pk2-free-v2.0.1/assets/js/moment.min.js"></script>
+    <script src="pk2-free-v2.0.1/assets/js/bootstrap-datetimepicker.min.js"></script>
+
+    <!--  Paper Kit Initialization snd functons -->
+    <script src="pk2-free-v2.0.1/assets/js/paper-kit.js?v=2.0.1"></script>
 </html>
