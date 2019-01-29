5494c876f20e585fee0a6e795445b9a670df56f3
D3algr4t <manu.gandul@gmail.com>
2019-01-29 12:09:03 +0100

    Add webservice

--- spil.controller/SpilController.php
+++ spil.controller/SpilController.php
@@ -1,14 +1,17 @@
 <?php
 
 interface SpilController {
-    
+
     function write($text, $idUser, $writeDate, $adultContent);
-    
+
     function listMsgs($idUser);
-    
+
     function report($pk);
-    
+
     function edit($idSpil, $text);
-    
+
     function delete($idSpil);
+
+    function read($idSpil);
+    
 }
