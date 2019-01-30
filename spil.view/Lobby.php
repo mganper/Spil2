a375679019fd56154c87c5e84b74b1872206c2ae
cauni <cauni@DESKTOP-280R1VQ>
2019-01-30 15:50:53 +0100

    Merge origin/master
    Conflicts:
    	spil.view/Like.php
    	spil.view/Lobby.php

--- spil.view/Lobby.php
+++ spil.view/Lobby.php
@@ -18,7 +18,7 @@ function array_sort_by(&$arrIni, $col, $order = SORT_DESC) {
 
 session_start();
 
-
+$_SESSION['usuario']='asd';
 if (isset($_SESSION['usuario'])) {
     $user = $_SESSION['usuario'];
 } else {
