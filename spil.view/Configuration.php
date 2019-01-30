396d07f3b02a13b0a0c9c608595ca50464ce73bb
cauni <cauni@DESKTOP-280R1VQ>
2019-01-30 13:45:46 +0100

    Merge origin/master
    Conflicts:
    	spil.view/User.php
    	spil.view/assets/js/scripting.js

--- spil.view/Configuration.php
+++ spil.view/Configuration.php
@@ -75,7 +75,7 @@ function soloImagenes($fichero) {
         <link rel="icon" type="image/png" href="assets/img/favicon.ico">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-        <title>Paper Kit 2 by Creative Tim</title>
+        <title>Spil | Configuracion</title>
 
         <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
         <meta name="viewport" content="width=device-width" />
--- spil.view/Lobby.php
+++ spil.view/Lobby.php
@@ -56,7 +56,7 @@ $numSpils = count($spils);
         <link rel="icon" type="image/png" href="assets/img/favicon.ico">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-        <title>Paper Kit 2 by Creative Tim</title>
+        <title>Spil | Inicio</title>
 
         <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
         <meta name="viewport" content="width=device-width" />
