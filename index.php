6cc69733b26da2d139d1c6d460ec80d1c42e774b
pabmunlop@gmail.com <pabmunlop@gmail.com>
2019-01-30 02:23:05 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 
 
-
 <?php
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '\Spil2\spil.controller\RespilControllerImpl.php';
--- spil.view/Configuration.php
+++ spil.view/Configuration.php
@@ -33,121 +33,127 @@ if (isset($_SESSION['usuario'])) {
             }
         }
     }
-} else {
-    header('Location: Login.php');
-}
+    if (isset($_POST['radioSensible'])) {
+        if (isset($_POST['sensible'])) {
+        
 
-function limiteTamanyo($fichero, $limite) {
-    return $fichero['size'] <= $limite;
-}
+            $confController->modifyConfiguration($_SESSION['usuario'], 0, 0, $_POST['sensible']);
+        }
+    } else {
+        header('Location: Login.php');
+    }
 
-function soloImagenes($fichero) {
-    $tiposAceptados = Array('image/gif', 'image/jpeg', 'image/pjpeg', 'image/png');
-    if (array_search($fichero['type'], $tiposAceptados) === false)
-        return false;
-    else
-        return true;
-}
-?>
+    function limiteTamanyo($fichero, $limite) {
+        return $fichero['size'] <= $limite;
+    }
 
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <link rel="icon" type="image/png" href="assets/img/favicon.ico">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    function soloImagenes($fichero) {
+        $tiposAceptados = Array('image/gif', 'image/jpeg', 'image/pjpeg', 'image/png');
+        if (array_search($fichero['type'], $tiposAceptados) === false)
+            return false;
+        else
+            return true;
+    }
+    ?>
 
-        <title>Paper Kit 2 by Creative Tim</title>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8" />
+            <link rel="icon" type="image/png" href="assets/img/favicon.ico">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-        <meta name="viewport" content="width=device-width" />
+            <title>Paper Kit 2 by Creative Tim</title>
 
-        <link href="pk2-free-v2.0.1/assets/css/bootstrap.min.css" rel="stylesheet" />
-        <link href="pk2-free-v2.0.1/assets/css/paper-kit.css?v=2.0.1" rel="stylesheet"/>
+            <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+            <meta name="viewport" content="width=device-width" />
 
-        <!--     Fonts and icons     -->
-        <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
-        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-        <link href="pk2-free-v2.0.1/assets/css/nucleo-icons.css" rel="stylesheet" />
+            <link href="pk2-free-v2.0.1/assets/css/bootstrap.min.css" rel="stylesheet" />
+            <link href="pk2-free-v2.0.1/assets/css/paper-kit.css?v=2.0.1" rel="stylesheet"/>
 
-        <style> .navbar {
-                margin-bottom: 0;
-                border-radius: 0;
-            }
+            <!--     Fonts and icons     -->
+            <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+            <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+            <link href="pk2-free-v2.0.1/assets/css/nucleo-icons.css" rel="stylesheet" />
 
-            /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-            .row.content {height: 450px}
+            <style> .navbar {
+                    margin-bottom: 0;
+                    border-radius: 0;
+                }
 
-            /* Set gray background color and 100% height */
-            .sidenav {
-                padding-top: 20px;
-                padding-bottom: 50%;
-                background-color: #f1f1f1;
-            }</style>
+                /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+                .row.content {height: 450px}
 
-    </head>
-    <body>
-        <!--    navbar come here          -->
-        <nav class="navbarnavbar-expand-md bg-info">
-            <div class="container" style="text-align: center;">         
-                <img src="pk2-free-v2.0.1/assets/img/spil_favicon_iz.png" style="max-width: 40px">          
-                <a class="navbar-brand nav-link" href="Lobby.php">Inicio</a>
-                <a class="navbar-brand nav-link" href="Notification.php">Notificaciones</a>
-                <a class="navbar-brand nav-link" href="User.php?user=<?php echo $user; ?>">Perfil</a>
-                <a class="navbar-brand nav-link" href="Configuration.php">Configuracion</a>
-                <button class="navbar-brand btn" data-toggle="modal" data-target="#MSGModal"style="margin: 5px; border: none; text-align: right; color: #00bbff; background-color: white;">Spilear</button>
-                <img src="pk2-free-v2.0.1/assets/img/spil_favicon_de.png" style="max-width: 40px; margin-left: 20px;">
-                <a class='navbar-brand nav-link navbar-right'href="Logout.php">Log out</a>
-            </div>
-        </nav>
-        <!-- end navbar  -->
+                /* Set gray background color and 100% height */
+                .sidenav {
+                    padding-top: 20px;
+                    padding-bottom: 50%;
+                    background-color: #f1f1f1;
+                }</style>
 
-        <div class="wrapper">
-            <div class="container-fluid text-center">    
-                <div class="row content" style="margin-top: 5px;">
-                    <div class="col-sm-2 sidenav">
-                        <div class="card-block col-sm-12" style="background-color: white; margin-top: 10px;">
-                            <div>
-                                <footer><h6>
-                                        © 
-                                        <script>document.write(new Date().getFullYear())</script>
-                                        , Grupo 10 Programacion Avanzada.
-                                    </h6></footer>
+        </head>
+        <body>
+            <!--    navbar come here          -->
+            <nav class="navbarnavbar-expand-md bg-info">
+                <div class="container" style="text-align: center;">         
+                    <img src="pk2-free-v2.0.1/assets/img/spil_favicon_iz.png" style="max-width: 40px">          
+                    <a class="navbar-brand nav-link" href="Lobby.php">Inicio</a>
+                    <a class="navbar-brand nav-link" href="Notification.php">Notificaciones</a>
+                    <a class="navbar-brand nav-link" href="User.php?user=<?php echo $user; ?>">Perfil</a>
+                    <a class="navbar-brand nav-link" href="Configuration.php">Configuracion</a>
+                    <button class="navbar-brand btn" data-toggle="modal" data-target="#MSGModal"style="margin: 5px; border: none; text-align: right; color: #00bbff; background-color: white;">Spilear</button>
+                    <img src="pk2-free-v2.0.1/assets/img/spil_favicon_de.png" style="max-width: 40px; margin-left: 20px;">
+                    <a class='navbar-brand nav-link navbar-right' href="Logout.php">Log out</a>
+                </div>
+            </nav>
+            <!-- end navbar  -->
+
+            <div class="wrapper">
+                <div class="container-fluid text-center">    
+                    <div class="row content" style="margin-top: 5px;">
+                        <div class="col-sm-2 sidenav">
+                            <div class="card-block col-sm-12" style="background-color: white; margin-top: 10px;">
+                                <div>
+                                    <footer><h6>
+                                            © 
+                                            <script>document.write(new Date().getFullYear())</script>
+                                            , Grupo 10 Programacion Avanzada.
+                                        </h6></footer>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-sm-8 text-center"> 
+                        <div class="col-sm-8 text-center"> 
 
-                        <h4 style="text-align: left">General</h4>
-                        <hr>
-                        <h5 style="text-align: left"><b>Mostrar contenido sensible:</b></h5><br>
+                            <h4 style="text-align: left">General</h4>
+                            <hr>
+                            <h5 style="text-align: left"><b>Mostrar contenido sensible:</b></h5><br>
 
-                        <form class="register-form"  style="text-align: left;" action="#">
-                            <div class="form-check-radio">
-                                <label class="form-check-label">
-                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" 
-                                    <?php
-                                    if ($config->isModoAdulto()) {
-                                        echo "checked";
-                                    }
-                                    ?>>
-                                    Sí
-                                    <span class="form-check-sign"></span>
-                                </label>
-                            </div>
-                            <div class="form-check-radio">
-                                <label class="form-check-label">
-                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"
-                                    <?php
-                                    if (!$config->isModoAdulto()) {
-                                        echo "checked";
-                                    }
-                                    ?>>
+                            <form class="register-form"  style="text-align: left;" action="#" method="post">
+                                <div class="form-check-radio">
+                                    <label class="form-check-label">
+                                        <input class="form-check-input" type="radio" name="sensible" id="exampleRadios1" value="1" 
+                                        <?php
+                                        if ($config->isModoAdulto()) {
+                                            echo "checked";
+                                        }
+                                        ?>>
+                                        Sí
+                                        <span class="form-check-sign"></span>
+                                    </label>
+                                </div>
+                                <div class="form-check-radio">
+                                    <label class="form-check-label">
+                                        <input class="form-check-input" type="radio" name="sensible" id="exampleRadios2" value="0"
+                                        <?php
+                                        if (!$config->isModoAdulto()) {
+                                            echo "checked";
+                                        }
+                                        ?>>
                                     No
                                     <span class="form-check-sign"></span>
                                 </label>
                             </div>
                             <br>
-                            <button class="btn btn-round bg-info">Enviar</button>
+                            <button id="radioSensible" type="submit" name="radioSensible" class="btn btn-round bg-info">Enviar</button>
                         </form>
                         <br>
                         <!-- FOTO PERFIL AQUÍ////////////////////////////////////////////////////////-->
@@ -205,28 +211,28 @@ function soloImagenes($fichero) {
                         </button>
                     </div>
                     <!--<form action="#">-->
-                        <div class="modal-body"> 
-                            <textarea class="form-control" rows="4" placeholder="Tell us your thoughts"></textarea>
-                            <label>Contenido sensible</label>
-                            <div class="form-check-radio">
-                                <label class="form-check-label">
-                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-                                    Off
-                                    <span class="form-check-sign"></span>
-                                </label>
-                            </div>
-                            <div class="form-check-radio">
-                                <label class="form-check-label">
-                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" >
-                                    On
-                                    <span class="form-check-sign"></span>
-                                </label>
-                            </div>
+                    <div class="modal-body"> 
+                        <textarea class="form-control" rows="4" placeholder="Tell us your thoughts"></textarea>
+                        <label>Contenido sensible</label>
+                        <div class="form-check-radio">
+                            <label class="form-check-label">
+                                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+                                Off
+                                <span class="form-check-sign"></span>
+                            </label>
                         </div>
-                        <div class="modal-footer">
-                            <button type="submit" class="btn btn-default btn-link" data-dismiss="modal">Publicar</button>
-                            <div class="divider"></div>                            
+                        <div class="form-check-radio">
+                            <label class="form-check-label">
+                                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" >
+                                On
+                                <span class="form-check-sign"></span>
+                            </label>
                         </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-default btn-link" data-dismiss="modal">Publicar</button>
+                        <div class="divider"></div>                            
+                    </div>
                     <!--</form>-->
                 </div>
             </div>
